6e6817c498c0d4e8f0bf4a9958ceaae74fb35ff7
Ketan Vaghela <ketanvaghela2230@gmail.com>
2023-01-13 11:13:21 +0530

    Rquest, Response error handler

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const apiRoutes = require('./routes')
-const {sequelize,connectToDb} = require('./db')
+const {sequelize,connectToDb} = require('./db');
+const { request, response } = require('express');
 // const bodyParser = require('body-parser');
 
 const app = express()
@@ -10,6 +11,16 @@ const PORT = process.env.port || 3022
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json())
 app.use('/api',apiRoutes)
+
+app.use((request, response) => {
+    response.status(404);
+    response.json({message:"Resource not found"});
+})
+
+app.use((request, response) => {
+    response.status(500);
+    response.json({message:"Oops... Something not found"});
+})
 
 app.listen(PORT, async ()=>{
     console.log(`App is runnig on http://localhost:${PORT}`);
